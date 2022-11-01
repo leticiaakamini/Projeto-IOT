669f7f1fd1a67d148adecb9f681ddb4a0d60e48a
Leticia Akamini <leticiaakamini@gmail.com>
2022-11-01 10:10:43 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MonitoracaoComponent } from './modules/home/pages/monitoracao/monitoracao.component';
+import { LoginComponent } from './modules/home/pages/login/login.component';
+import { NotificacaoComponent } from './modules/home/pages/notificacao/notificacao.component';
+import { SetoresComponent } from './modules/home/pages/setores/setores.component';
+
+const routes: Routes = [
+  {path: '', component: MonitoracaoComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'notificacao', component: NotificacaoComponent},
+  {path: 'setores', component: SetoresComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
